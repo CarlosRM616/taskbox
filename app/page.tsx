3b8836b8f0e0ae7b99ee04354089a4d617c3f7b4
CarlosRM616 <ramc010616@gmail.com>
2026-08-25 16:00:04 -0600

    Probar preview deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 export default function Home() {
   return (
-    <h1>
-      Mi primera aplicación desplegada con Vercel.
-      Y Samuel es un manco junto con todos los de los semestres anteriores
-      y compañeros.
-    </h1>
+    <h1>Esta es mi Preview Deployment</h1>
   );
 }
