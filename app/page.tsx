09164471c2b7772a50f82fe7bd321f96dba31634
CarlosRM616 <ramc010616@gmail.com>
2026-08-25 14:37:52 -0600

    Actualizar el título de TaskBox

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ export default function Home() {
   return (
     <h1>
       Mi primera aplicación desplegada con Vercel.
+      Y Samuel es un manco junto con todos los de los semestres anteriores
+      y compañeros.
     </h1>
   );
 }
